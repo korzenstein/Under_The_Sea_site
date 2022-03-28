e1f96bb8ebf0650c4ea9af91a333b357838a5a96
Stephen Korzenstein <skorzenstein@gmail.com>
2022-03-27 20:16:38 -0400

    created API function for profile pics

--- script/main.js
+++ script/main.js
@@ -1,25 +1,51 @@
-let nav = document.querySelector('.nav');
-
-// Toggle Button Functionality
+// Navigation Toggle Button Functionality (from Project 1)
+const nav = document.querySelector('.nav');
 function navBar() {
     nav.classList.toggle('active');
 }
 
-// Assignment 2 Functionality
-
+// Assignment 2 Functionality - Add Comments to Blog
+// ----------------
 
 // Declare variables for DOM elements
-let commentForm = document.getElementById('commentForm');
-let commentsContainer = document.querySelector('.commentsContainer')
+const commentForm = document.getElementById('commentForm');
+const commentsContainer = document.querySelector('.commentsContainer')
+const image = document.createElement('img');
+
+// Async API call that places random profile image for commenter photo
+const fetchPortrait = async () => {
+    const data = await fetch(`https://randomuser.me/api/`);
+    const portraitData = await data.json();
+    image.setAttribute("src", portraitData.results[0].picture.large);
+    image.setAttribute("alt", "Portrait of submitter")
+    image.classList.add('commenterImg')
+    image.style.width="100%";
+}
+fetchPortrait()
 
 // Comment Form Functionality
 commentForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    let submit = document.querySelector('.submit')
-    let email = document.getElementById('email').value
-    let name = document.getElementById('name').value
-    let message = document.getElementById('message').value
+    const email = document.getElementById('email').value
+    const name = document.getElementById('name').value
+    const message = document.getElementById('message').value
+    
 
+    // Error handling to check for name, email and comment
+    if(!name) {
+        alert('This is not a valid name')
+        return ""
+    }
+
+    if(!email) {
+        alert('This is not a valid email')
+        return ""
+    }
+
+    if(!message) {
+        alert('This is not a valid blog entry')
+        return ""
+    }
 
     // Creates our date timestamp
     const date = `${new Date().toLocaleString("en-US", { day: "2-digit" })}`;
@@ -28,43 +54,38 @@ commentForm.addEventListener('submit', function(event) {
     const year = `${new Date().getFullYear()}`;
 
     // Creates a new Li Element
-    let newComment = document.createElement('li')
+    const newComment = document.createElement('li')
     newComment.classList.add('comments')
 
     // Creates a new Div container for our Img
-    let imageDiv = document.createElement('div')
+    const imageDiv = document.createElement('div')
     imageDiv.classList.add('commenterImg')
-
-    // Creates a new Img and sets attributes
-    let image = document.createElement('img');
-    image.setAttribute("src", "../assets/comment1.jpg");
-    image.setAttribute("alt", "Portrait of submitter")
-    image.classList.add('commenterImg')
-    image.style.width="100%";
 
     // Lets place that image in its container Div
     imageDiv.append(image);
     
     // Creates the Div for the comments
-    let commentNotes = document.createElement('div')
+    const commentNotes = document.createElement('div')
     commentNotes.classList.add('commentNotes')
 
     // Creates the paragraph for it
-    let para = document.createElement('p')
+    const para = document.createElement('p')
     para.innerText = `${day} ${month} ${date}, ${year} by ${name}`
 
     // Creates the blockquote for comments
-    let blockquote = document.createElement('blockquote')
+    const blockquote = document.createElement('blockquote')
     blockquote.innerText = `${message}`
 
-    // Places p and blockq into their container
+    // Places p and blockquote into their container
     commentNotes.append(para, blockquote);
 
     // Places 2 main Divs into Li
     newComment.append(imageDiv, commentNotes)
 
     // Places Li into Section
-    commentsContainer.appendChild(newComment)    
+    commentsContainer.appendChild(newComment) 
+
 })
 
-console.log(commentsContainer)
+
+
